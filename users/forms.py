9b81d34de4a50d5d25bd0fb85cab542d6d741224
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2022-10-12 21:10:08 +0530

    Profile

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import ProfileModel
 
 class UserForm(UserCreationForm):
     first_name = forms.CharField()
@@ -9,3 +10,28 @@ class UserForm(UserCreationForm):
     class Meta:
        model = User
        fields = ('first_name','last_name', 'username', 'email', 'password1' ,'password2' )
+    def __init__(self,*args, **kwargs):
+        super(UserForm,self).__init__(*args,**kwargs)
+        for filedname in ['first_name','last_name', 'username', 'email', 'password1' ,'password2']:
+            self.fields[filedname].help_text = None
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name','last_name','username','email']
+    
+    def __init__(self,*args, **kwargs):
+        super(UserUpdateForm,self).__init__(*args,**kwargs)
+        for filedname in ['first_name','last_name','username','email']:
+            self.fields[filedname].help_text = None
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = ProfileModel
+        fields = ['image']
+
+
+
+
+
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render  
 from django.contrib.auth.forms import UserCreationForm  
-from .forms import UserForm
+from .forms import UserForm,UserUpdateForm,ProfileUpdateForm
 # Create your views here.  
   
 def sign_up(request):
@@ -16,4 +16,25 @@ def sign_up(request):
     return render(request, 'users/sign_up.html', {'form': form}) 
 
 def profile(request):
-    return render(request,'users/profile.html')
+    if request.method == 'POST':
+        u_form =  UserUpdateForm(request.POST or None ,instance=request.user)
+        p_form = ProfileUpdateForm(request.POST or None,request.FILES or None,instance=request.user.profilemodel)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            return redirect('users-profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profilemodel)
+
+    context = {
+        'u_form':u_form,
+        'p_form':p_form
+    }
+    return render(request,'users/profile.html',context)
+
+
+
+
+
+
